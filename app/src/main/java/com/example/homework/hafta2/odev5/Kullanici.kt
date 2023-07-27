bffd40918295b9f57803b9b7a898f670eff11b7f
tahaaltuntasdev <m.tahaaltuntas@gmail.com>
2023-07-27 23:51:12 +0300

    hafta 2 - odev 5

---
+++ app/src/main/java/com/example/homework/hafta2/odev5/Kullanici.kt
@@ -0,0 +1,8 @@
+package com.example.homework.hafta2.odev5
+
+class Kullanici (var ad: String? = null, var soyad: String? = null) {
+    fun adSoyad() {
+        println("Ad: $ad")
+        println("Soyad: $soyad")
+    }
+}
---
+++ app/src/main/java/com/example/homework/hafta2/odev5/MainKullanici.kt
@@ -0,0 +1,7 @@
+package com.example.homework.hafta2.odev5
+
+fun main(){
+    val kullanici : Kullanici
+    kullanici = Kullanici("Taha","Altuntaş")
+    kullanici.adSoyad()
+}
