f97477be957600bfb80e0e523b5a85618e74376c
tahaaltuntasdev <m.tahaaltuntas@gmail.com>
2023-07-27 23:55:30 +0300

    hafta 2 - odev 6

---
+++ app/src/main/java/com/example/homework/hafta2/odev6/NullSafety.kt
@@ -0,0 +1,9 @@
+package com.example.homework.hafta2.odev6
+
+/*Null referanslara karşı güvenlik: Kotlin, null referanslarla ilgili sorunları minimize ederek programlarda null
+pointer exception (NPE) hatalarını önlemeye yardımcı olur. Bu tür hatalar, bir nesnenin null olabileceği varsayımı
+yapılmadan dolayı oluşur. Kotlin, null olabilen değerleri açıkça tanımlayarak bu tür hataların önüne geçer.
+Önceden tanımlanmış null değerler: Kotlin, null değeri temsil etmek için özel bir tip olan "Nullable" tipini kullanır.
+Bu, null değeriyle ilgili işlemleri daha açık bir şekilde gerçekleştirmemizi sağlar. Aynı zamanda, güvenli bir şekilde null değerlerini
+kontrol etmek için null olmayan türleri dönüştürmek için null-checklerin kullanımını teşvik eder.
+ */
